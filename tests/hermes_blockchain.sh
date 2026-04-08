c480dd76afd241478e492992d1c820ba0c705560
AncientEncoder <vabismo452@gmail.com>
2026-04-08 16:16:33 +0200

    Update hermes_blockchain.sh

--- tests/hermes_blockchain.sh
+++ tests/hermes_blockchain.sh
@@ -1,7 +1,9 @@
 mkdir tests/test_hermes
 cd tests/test_hermes
+pwd
 wget https://github.com/informalsystems/hermes/releases/download/v1.13.3/hermes-v1.13.3-x86_64-unknown-linux-gnu.tar.gz
 tar xzvf hermes-v1.13.3-x86_64-unknown-linux-gnu.tar.gz
 cd ../..
+pwd
 python3 src/main.py binary -b tests/test_hermes/hermes-v1.13.3-x86_64-unknown-linux-gnu/hermes
 rm -rf tests/test_hermes
